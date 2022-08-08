d7978bef429923550344d487742cbfa84469ef0c
tmoSolutions <timo.heinkel@gmail.com>
2022-08-08 18:34:41 +0200

    connected new sliders to params

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -44,6 +44,18 @@ private:
         lowCutSlopeSlider,
         highCutSlopeSlider;
 
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachement = APVTS::SliderAttachment;
+
+    Attachement peakFreqSliderAttachement,
+        peakGainSliderAttachement,
+        peakQualitySliderAttachement,
+        lowCutFreqSliderAttachement,
+        highCutFreqSliderAttachement,
+        lowCutSlopeSliderAttachement,
+        highCutSlopeSliderAttachement;
+
+
     std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimplEQAudioProcessorEditor)
