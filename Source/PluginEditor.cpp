1b2769d054ca6b071f341e36900014551ded0b6e
tmoSolutions <timo.heinkel@gmail.com>
2022-08-12 18:41:05 +0200

    dialed in GUI positioning

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -334,7 +334,7 @@ SimplEQAudioProcessorEditor::SimplEQAudioProcessorEditor(SimplEQAudioProcessor& 
         addAndMakeVisible(comp);
     }
 
-    setSize (600, 400);
+    setSize (600, 480);
 }
 
 SimplEQAudioProcessorEditor::~SimplEQAudioProcessorEditor()
@@ -356,9 +356,12 @@ void SimplEQAudioProcessorEditor::resized()
     // subcomponents in your editor..
     
     auto bounds = getLocalBounds();
-    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+    float hRatio = 25.f / 100.f; //JUCE_LIVE_CONSTANT(33) / 100.f; is able to live show the effect when changing the value in parantheses
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * hRatio);
 
     responseCurveComponent.setBounds(responseArea);
+
+    bounds.removeFromTop(5);
 
     auto lowCutArea = bounds.removeFromLeft(bounds.getWidth() * 0.33);
     auto highCutArea = bounds.removeFromRight(bounds.getWidth() * 0.5);
