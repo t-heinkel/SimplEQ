b3e3abd3f990dcce75b93d3519a9b4fc64401363
tmoSolutions <timo.heinkel@gmail.com>
2022-08-13 15:07:47 +0200

    added Analyzer dB marks

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -394,9 +394,18 @@ void ResponseCurveComponent::resized()
         r.setSize(textWidth, fontHeight);
         r.setX(getWidth() - textWidth);
         r.setCentre(r.getCentreX(), y);
-
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey);
         g.drawFittedText(str, r, juce::Justification::centred, 1);
+
+        str.clear();
+        str << (gDb - 24.f);
+
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+
     }
 }
 
